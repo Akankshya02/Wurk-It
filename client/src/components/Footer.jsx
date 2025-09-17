ab1b5de02821ab5f1c8069a1c3fc7ab8740bbf37
Akankshya02 <akankshyapattnaik5@gmail.com>
2025-09-17 20:57:26 +0530

    job listing part completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='contaier px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20'>
+      <img width={160} src={assets.logo} alt="" />
+      <p className='flex-l border-l border0gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>© 2025 Wurk It. All rights reserved.</p>
+      <div className='flex gap-2.5'>
+        <img width={38} src={assets.facebook_icon} alt="" />
+        <img width={38} src={assets.instagram_icon} alt="" />
+        <img width={38} src={assets.twitter_icon} alt="" />
+      </div>
     </div>
   )
 }
